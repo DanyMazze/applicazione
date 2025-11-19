f3aa9b588c7c72aea04f82394708f170135908db
DanyMazze <daniele.mazzetti08@gmail.com>
2025-11-19 10:34:03 +0100

    finito

--- applicazione_notebook/app.js
+++ applicazione_notebook/app.js
@@ -1,19 +1,45 @@
 const { createApp } = Vue;
 
 const app = createApp({
-data() {
-    return {
-        islogged: false
-    };
-},
-methods: {
-    togglelogin() {
-        this.islogged = true;
+    data() {
+        return {
+            islogged: false,
+            username: '',
+            userOk: false,
+            emailOk: false,
+            canLogin: false,
+            email: '',
+            newNote: '',
+            notes: []
+        };
+    },
+    methods: {
+        togglelogin() {
+            if (!this.islogged) {
+                if (!this.canLogin) return;
+            }
+            this.islogged = !this.islogged;
+        },
+    },
+    watch: {
+        username(newvalue){
+            if (newvalue.length < 0){
+                this.userOk = false;
+            }
+        },
+        email(newvalue){
+            if (newvalue.includes('@')){
+                this.emailOk = true;
+            } else {
+                this.emailOk = false;
+            }
+        },
+        canLogin() {
+            if (this.userOk && this.emailOk) {
+                this.canLogin = true;
+            }
+        }
     }
-},
-watch: {
-    
-}
 });
 
 app.mount('#app');
