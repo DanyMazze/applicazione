bc8951d0f9a6e7238ad8cdb45763d4a9f081f2eb
Daniele Mazzetti <Daniele.mazzetti08@gmail.com>
2025-11-20 13:42:58 +0100

    Update app.js

--- applicazione_notebook/app.js
+++ applicazione_notebook/app.js
@@ -6,10 +6,10 @@ const app = createApp({
             islogged: false,
             email: '',
             isFormValid: false,
-            newNote: {
-                title: '',
-                description: ''
-            }
+            username: '',
+            newNotetitle: '',
+            newNotedescription: '',
+            notes: []
         };
     },
     watch: {
@@ -23,21 +23,41 @@ const app = createApp({
                 this.islogged = true;
             }
         },
-        addNote(newNote) {
-            this.newNote.title = '';
-            this.newNote.description = '';
-            
+        addNote() {
+            if (this.newNotetitle && this.newNotedescription) {
+                this.notes.push({
+                    title: this.newNotetitle,
+                    description: this.newNotedescription
+                });
+                this.newNotetitle = '';
+                this.newNotedescription = '';
+            } else {
+                alert('Titolo e descrizione sono obbligatori!');
+            }
+        },
+        deleteNote(noteToDelete) {
+            const index = this.notes.indexOf(noteToDelete);
+            if (index !== -1) {
+                this.notes.splice(index, 1); 
+            }
         }
     }
 });
 
 app.component('note-card', {
-    props: ['titolo', 'descrizione'],
+    props: ['titolo', 'descrizione', 'note', 'onDelete'], 
     template: `
         <div class="note">
             <h2>{{ titolo }}</h2>
             <p>{{ descrizione }}</p>
+            <button @click="deleteNote">Elimina Nota</button> 
         </div>
-    `
+    `,
+        methods: {
+        deleteNote() {
+            this.onDelete(this.note);  
+        }
+    }
 });
+
 app.mount('#app');
