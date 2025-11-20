3a7437b4381c285ebd4e29c28aa1a35f8038d72b
DanyMazze <daniele.mazzetti08@gmai.com>
2025-11-20 08:08:14 +0100

    fatto

--- applicazione_notebook/app.js
+++ applicazione_notebook/app.js
@@ -1,19 +1,43 @@
 const { createApp } = Vue;
 
 const app = createApp({
-data() {
-    return {
-        islogged: false
-    };
-},
-methods: {
-    togglelogin() {
-        this.islogged = true;
+    data() {
+        return {
+            islogged: false,
+            email: '',
+            isFormValid: false,
+            newNote: {
+                title: '',
+                description: ''
+            }
+        };
+    },
+    watch: {
+        email(newVal) {
+            this.isFormValid = newVal.includes('@');
+        }
+    },
+    methods: {
+        togglelogin() {
+            if (this.isFormValid) {
+                this.islogged = true;
+            }
+        },
+        addNote(newNote) {
+            this.newNote.title = '';
+            this.newNote.description = '';
+            
+        }
     }
-},
-watch: {
-    
-}
 });
 
+app.component('note-card', {
+    props: ['titolo', 'descrizione'],
+    template: `
+        <div class="note">
+            <h2>{{ titolo }}</h2>
+            <p>{{ descrizione }}</p>
+        </div>
+    `
+});
 app.mount('#app');
